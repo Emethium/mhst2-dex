9d9f8de7568b564854c3789ac8fa50c0e9694127
emethium <nikolas.serafini@gmail.com>
2021-07-20 14:41:02 -0300

    feat: react project init

---
+++ web/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Alert } from 'reactstrap';
+
+export default function App() {
+  console.log('hello')
+  return (
+    <div>
+      <Alert color="success">
+        This is still a work in progress, do not take this too seriously
+      </Alert>
+    </div>
+  );
+}
+
