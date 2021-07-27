2b9f02e778c8913a3972b229f167501639361630
emethium <nikolas.serafini@gmail.com>
2021-07-27 19:44:12 +0000

    feat(web): added basic layout for web root page

--- web/App.tsx
+++ web/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import { Alert } from 'reactstrap';
+import React from "react";
+import { Alert, Container, Row } from "reactstrap";
+
+import CustomNavbar from "./components/navbar/CustomNavbar";
+import MonsterCard from "./components/monsterCard/MonsterCard";
 
 export default function App() {
-  console.log('hello')
+  const monster = {
+    id: 1,
+    name: "Anjanath",
+    type: "Power",
+    regionFound: "Loloska Forest",
+    portraitURL: "https://cdn.kiranico.net/file/kiranico/mhstories-web/micons/micon136.png",
+    eggURL: "https://cdn.kiranico.net/file/kiranico/mhstories-web/eggs/buddy_140.png"
+  }
+
   return (
     <div>
-      <Alert color="success">
-        This is still a work in progress, do not take this too seriously
-      </Alert>
+      <div>
+        <CustomNavbar />
+        <Alert color="warning">
+          This is still a work in progress, do not take this too seriously
+        </Alert>
+      </div>
+
+      <div>
+        <MonsterCard {...monster} />
+      </div>
     </div>
   );
 }
-
