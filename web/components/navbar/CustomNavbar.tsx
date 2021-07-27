8d8e47932ef6020212db268506963fb32fab3c5f
emethium <nikolas.serafini@gmail.com>
2021-07-27 19:42:36 +0000

    feat(components): created custom navbar

---
+++ web/components/navbar/CustomNavbar.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from "reactstrap";
+
+export default function CustomNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen)
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">mhst2-dex</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/about">About</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
