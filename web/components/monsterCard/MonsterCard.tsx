16ea9d4d813e1615a69b7db7f1819a4b0913af8a
emethium <nikolas.serafini@gmail.com>
2021-07-29 13:21:32 +0000

    feat(components): added more complete version of the monster card component

--- web/components/monsterCard/MonsterCard.tsx
+++ web/components/monsterCard/MonsterCard.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import { Card, CardBody, CardImg, CardSubtitle, CardText, CardTitle, Col, Media, Row } from 'reactstrap'
-import { Monster } from '../../domain/Monster'
+import { Card, CardBody, CardSubtitle, CardText, CardTitle, Col, Media, Row } from 'reactstrap'
+import { Monster } from '../../domain'
 
 export default function MonsterCard(props: Monster) {
   return (
-    <div>
-      <Card>
-        <CardImg top width="auto" src={props.portraitURL} alt={`${props.name} portrait image`} />
+      <Card id={props.name}>
         <CardBody>
           <Row>
             <Col>
@@ -15,13 +13,19 @@ export default function MonsterCard(props: Monster) {
               </Media>
             </Col>
             <Col>
-              <CardTitle tag="h5">{props.name}</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">{props.type}</CardSubtitle>
-              <CardText>Found in {props.regionFound} region</CardText>
+              <Row>
+                <Media left href="#">
+                  <Media src={props.portraitURL} alt={`${props.name} portrait image`}/>
+                </Media>
+                <CardTitle tag="h5">{props.name}</CardTitle>
+              </Row>
+              <Row>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">{props.type}</CardSubtitle>
+                <CardText>Found in {props.regionFound} region</CardText>
+              </Row>
             </Col>
           </Row>
         </CardBody>
       </Card>
-    </div>
   )
 }
